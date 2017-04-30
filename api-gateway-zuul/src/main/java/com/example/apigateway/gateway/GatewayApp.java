b5721202ee680bd11134bad052a5cdb8fc7381ad
kubamarchwicki <kuba@marchwicki.pl>
2017-04-30 18:26:19 +0200

    Service discovery: solution: registration of Zuul based api gateway

--- api-gateway-zuul/src/main/java/com/example/apigateway/gateway/GatewayApp.java
+++ api-gateway-zuul/src/main/java/com/example/apigateway/gateway/GatewayApp.java
@@ -4,6 +4,7 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +21,7 @@ import java.util.List;
 
 @EnableZuulProxy
 @SpringBootApplication
-//TODO: @EnableDiscoveryClient
+@EnableDiscoveryClient
 public class GatewayApp {
 
     public static void main(String[] args) {
